73f061bd35ddaa2b3126472c64c2e07d4b97302a
artemagant <al4166964@gmail.com>
2026-03-15 21:57:30 +0400

    Add player sprite

--- isak/Scenes/Player/player.gd
+++ isak/Scenes/Player/player.gd
@@ -2,19 +2,23 @@ extends CharacterBody2D
 
 var direction: Vector2 = Vector2(0, 0)
 
-var speed = 350
-
+var speed = 1
+var speed_mult = 100
+@onready var animation := $AnimatedSprite2D
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("Left"):
 		direction.x += -1
+		animation.play("Move_Left")
 	if event.is_action_pressed("Down"):
 		direction.y += 1
+		animation.play("Move_Down")
 	if event.is_action_pressed("Right"):
 		direction.x += 1
+		animation.play("Move_Right")
 	if event.is_action_pressed("up"):
 		direction.y += -1
-	
+		animation.play("Move_Up")
 	if event.is_action_released("Left"):
 		direction.x -= -1
 	if event.is_action_released("Down"):
@@ -23,7 +27,9 @@ func _input(event: InputEvent) -> void:
 		direction.x -= 1
 	if event.is_action_released("up"):
 		direction.y -= -1
-		
+	
+	if direction == Vector2(0, 0):
+		animation.play("Idle")
 func _physics_process(delta: float) -> void:
-	position += direction * speed * delta
+	position += direction.normalized() * speed * speed_mult * delta
 	move_and_slide()
