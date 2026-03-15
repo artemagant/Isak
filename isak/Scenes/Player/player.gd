cead594bcaa8a0f9de55eee81f6f4063c72a6037
artemagant <al4166964@gmail.com>
2026-03-16 01:55:58 +0400

    Update player sprites. Add mobile support

--- isak/Scenes/Player/player.gd
+++ isak/Scenes/Player/player.gd
@@ -2,23 +2,30 @@ extends CharacterBody2D
 
 var direction: Vector2 = Vector2(0, 0)
 
-var speed = 1
-var speed_mult = 100
-@onready var animation := $AnimatedSprite2D
+var speed: = 2.5
+var speed_mult: = 100
+@onready var animation := $Bottom
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("Left"):
 		direction.x += -1
-		animation.play("Move_Left")
+		animation.flip_h = false
+		animation.sprite_frames.set_animation_speed("Move_Run", max(speed, 2))
+		animation.play("Move_Run")
 	if event.is_action_pressed("Down"):
 		direction.y += 1
-		animation.play("Move_Down")
+		animation.sprite_frames.set_animation_speed("Move_Run",  max(speed, 2))
+		animation.play("Move_Run")
 	if event.is_action_pressed("Right"):
 		direction.x += 1
-		animation.play("Move_Right")
+		animation.flip_h = true
+		animation.sprite_frames.set_animation_speed("Move_Run",  max(speed, 2))
+		animation.play("Move_Run")
 	if event.is_action_pressed("up"):
 		direction.y += -1
-		animation.play("Move_Up")
+		animation.sprite_frames.set_animation_speed("Move_Run", max(speed, 2))
+		animation.play("Move_Run")
+	
 	if event.is_action_released("Left"):
 		direction.x -= -1
 	if event.is_action_released("Down"):
@@ -29,6 +36,7 @@ func _input(event: InputEvent) -> void:
 		direction.y -= -1
 	
 	if direction == Vector2(0, 0):
+		animation.flip_h = false
 		animation.play("Idle")
 func _physics_process(delta: float) -> void:
 	position += direction.normalized() * speed * speed_mult * delta
